e801372b9fbfac13d8c1084a471db71aa7cef466
fatih ozdil <fatih.ozdil07@gmail.com>
2023-05-24 09:55:14 +0300

    add crud to file object

---
+++ Core/Features/Files/Queries/GetAllFiles/GetAllFilesViewModel.cs
@@ -0,0 +1,10 @@
+namespace Core.Features.Files.Queries.GetAllFiles
+{
+    public class GetAllFilesViewModel
+    {
+
+        public string UserId { get; set; }
+        public string Path { get; set; }
+        public string Content { get; set; }
+    }
+}
--- Core/Mappings/GeneralProfile.cs
+++ Core/Mappings/GeneralProfile.cs
@@ -1,7 +1,9 @@
 ﻿using AutoMapper;
 using Core.Entities;
 using Core.Features.Products.Commands.CreateProduct;
+using Core.Features.Files.Commands.CreateFile;
 using Core.Features.Products.Queries.GetAllProducts;
+using Core.Features.Files.Queries.GetAllFiles;
 
 namespace Core.Mappings
 {
@@ -12,6 +14,10 @@ namespace Core.Mappings
             CreateMap<Product, GetAllProductsViewModel>().ReverseMap();
             CreateMap<CreateProductCommand, Product>();
             CreateMap<GetAllProductsQuery, GetAllProductsParameter>();
+
+            CreateMap<File, GetAllFilesViewModel>().ReverseMap();
+            CreateMap<CreateFileCommand, File>();
+            CreateMap<GetAllFilesQuery, GetAllFilesParameter>();
         }
     }
 }
