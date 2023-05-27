640c4225e7305240790f29f934d24cb117a2b74d
arinmis <mustafa_arinmis@outlook.com>
2023-05-27 17:03:20 +0300

    Add API descriptions

--- WebApi/Controllers/FileController.cs
+++ WebApi/Controllers/FileController.cs
@@ -20,6 +20,16 @@ namespace WebApi.Controllers.v1
     public class FileController : BaseApiController
     {
 
+        /// <summary>
+        /// Returns specific file created by Authorized user
+        /// </summary>
+        /// <remarks>
+        /// Sample request:
+        /// 
+        ///     GET api/file/foo/bar.py 
+        ///     {        
+        ///     }
+        /// </remarks>
         [HttpGet("{path}")]
         public async Task<IActionResult> GetFile(string path)
         {
@@ -29,28 +39,70 @@ namespace WebApi.Controllers.v1
             }));
         }
 
-
+        /// <summary>
+        /// Returns all of the file created by Authorized user
+        /// </summary>
+        /// <returns>The requested item.</returns>
         [HttpGet("")]
         public async Task<IActionResult> Get()
         {
             return Ok(await Mediator.Send(new GetAllFiles()));
         }
 
-        // POST api/<controller>
+        /// <summary>
+        /// Creates file with  file path and content for  Authorized user
+        /// </summary>
+        /// <remarks>
+        /// Sample request:
+        /// 
+        ///     POST api/file/foo/bar.py 
+        ///     {        
+        ///         "path": "/foo/bar.py",
+        ///         "content": "random_integer = random.randint(1, 100)"
+        ///     }
+        /// </remarks>
         [HttpPost]
         public async Task<IActionResult> Post(CreateFileCommand command)
         {
             return Ok(await Mediator.Send(command));
         }
 
-        // PUT api/<controller>/5
+        /// <summary>
+        /// Updates file path and content for  Authorized user
+        /// </summary>
+        /// <remarks>
+        /// To update file path, pass existing content and change new path:
+        ///     PUT api/file/foo/bar.py 
+        ///      {
+        ///         "oldPath": "/foo/bar.py",
+        ///         "newPath": "/foo/baz.txt",
+        ///         "content": "random_integer = random.randint(1, 100)"
+        ///      }
+        /// To update file content, use same file path and just change content:
+        /// 
+        ///     PUT api/file/foo/bar.py 
+        ///      {
+        ///         "oldPath": "/foo/bar.py",
+        ///         "newPath": "/foo/bar.py",
+        ///         "content": "random_integer = [randint(0, 9) for p in range(0, 10)]"
+        ///      }
+        /// </remarks>
         [HttpPut("")]
         public async Task<IActionResult> Put(UpdateFileCommand command)
         {
             return Ok(await Mediator.Send(command));
         }
 
-        // DELETE api/<controller>/5
+        /// <summary>
+        /// Deletes file that has given path for  Authorized user
+        /// </summary>
+        /// <remarks>
+        /// Sample request:
+        /// 
+        ///     DELETE api/file/foo/bar.py 
+        ///     {        
+        ///     }
+        /// </remarks>
         [HttpDelete("{path}")]
         public async Task<IActionResult> Delete(string path)
         {
