f01d28f5b60d1d6cd6f5a97173935fcd874c3154
arinmis <mustafa_arinmis@outlook.com>
2023-06-15 14:07:10 +0300

    Add seed users

--- Infrastructure/Seeds/DefaultBasicUser.cs
+++ Infrastructure/Seeds/DefaultBasicUser.cs
@@ -11,7 +11,7 @@ namespace Infrastructure.Seeds
         public static async Task SeedAsync(UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
         {
             //Seed Default User
-            var defaultUser = new ApplicationUser
+            var arinmisUser = new ApplicationUser
             {
                 UserName = "arinmis",
                 Email = "mustafa_arinmis@outlook.com",
@@ -20,16 +20,61 @@ namespace Infrastructure.Seeds
                 EmailConfirmed = true,
                 PhoneNumberConfirmed = true
             };
-            if (userManager.Users.All(u => u.Id != defaultUser.Id))
+            if (userManager.Users.All(u => u.Id != arinmisUser.Id))
             {
-                var user = await userManager.FindByEmailAsync(defaultUser.Email);
+                var user = await userManager.FindByEmailAsync(arinmisUser.Email);
                 if (user == null)
                 {
-                    await userManager.CreateAsync(defaultUser, "Arinmis123.");
-                    await userManager.AddToRoleAsync(defaultUser, Roles.Basic.ToString());
+                    await userManager.CreateAsync(arinmisUser, "Arinmis123.");
+                    await userManager.AddToRoleAsync(arinmisUser, Roles.Basic.ToString());
                 }
 
             }
+
+
+
+            var yusufUser = new ApplicationUser
+            {
+                UserName = "selamYusufBen",
+                Email = "yusuf@gmail.com",
+                FirstName = "yusuf",
+                LastName = "tanrikulu",
+                EmailConfirmed = true,
+                PhoneNumberConfirmed = true
+            };
+            if (userManager.Users.All(u => u.Id != yusufUser.Id))
+            {
+                var user = await userManager.FindByEmailAsync(yusufUser.Email);
+                if (user == null)
+                {
+                    await userManager.CreateAsync(yusufUser, "Yusuf123.");
+                    await userManager.AddToRoleAsync(yusufUser, Roles.Basic.ToString());
+                }
+
+            }
+
+
+            var testUser = new ApplicationUser
+            {
+                UserName = "testtest",
+                Email = "test@test.com",
+                FirstName = "test",
+                LastName = "test",
+                EmailConfirmed = true,
+                PhoneNumberConfirmed = true
+            };
+            if (userManager.Users.All(u => u.Id != testUser.Id))
+            {
+                var user = await userManager.FindByEmailAsync(testUser.Email);
+                if (user == null)
+                {
+                    await userManager.CreateAsync(testUser, "Testtest1.");
+                    await userManager.AddToRoleAsync(testUser, Roles.Basic.ToString());
+                }
+
+            }
+
+
         }
     }
 }
